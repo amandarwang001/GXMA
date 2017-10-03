f6aba108cd4251139bcf1ba912d93a0e3e860936
Amanda Wang <amandawang@dijatek.com>
2017-10-03 10:51:46 -0400

    changes for heroku

--- resources/config.php
+++ resources/config.php
@@ -26,8 +26,8 @@ if ($_SERVER['HTTP_HOST'] == 'guoxingmartialarts.com') {
      $basePath = $_SERVER['DOCUMENT_ROOT'] . '/';
      $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
      $baseUrl = $protocol . 'gxma.herokuapp.com/';
-//     define('CSS_URL', 'assets/css/');
-//     define('JS_URL',  'assets/js/');
+     define('CSS_URL', 'assets/css/');
+     define('JS_URL',  'assets/js/');
  }
 
 // Set commonly used paths
@@ -39,8 +39,8 @@ define('LANDING_INCLUDES_PATH', INCLUDES_PATH . 'landingpages/');
 //Path for blog
 define('BLOG_PATH', $basePath . 'blog/');
 // Set common links for localhost
-//define('CSS_URL', 'assets/css/');
-//define('JS_URL',  'assets/js/');
+define('CSS_URL', 'assets/css/');
+define('JS_URL',  'assets/js/');
 
 
 
